366b9cf1f0adfcade830f21bfa1db280589b9521
AMJAD MM <166852555+amjad-mm@users.noreply.github.com>
2026-08-03 23:32:37 +0530

    Time: 10 ms (30.27%), Space: 62.9 MB (87.08%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+
+    if(x < 0 || (x % 10 === 0 && x !== 0)){
+        return false;
+    }
+
+    let halfRev = 0;
+
+    while(halfRev < x){
+
+        halfRev = halfRev * 10 + (x % 10);
+        x = Math.floor(x / 10);
+    }
+
+    return halfRev === x || Math.floor(halfRev / 10) === x;
+};
